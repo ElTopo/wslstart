490dd21b60e5a4125b1fc920cb1b7b16c6e5ee77
El Topo <eltopo@live.com>
2019-01-17 14:57:30 -0800

    update wslstart.cmd: 1. use new wsl.exe command (replacing old bash.exe); 2. comment on smaller cmd window

--- wslstart.cmd
+++ wslstart.cmd
@@ -1,5 +1,12 @@
 @echo off
+
+@rem use smaller window
 @mode con:cols=66 lines=6
 @set PROMPT=# 
 
-@bash -c "/usr/sbin/wslstart"
+@rem old version: use bash.exe
+@rem bash -c "/usr/sbin/wslstart"
+
+@rem new version: use wsl.exe
+@wsl /usr/sbin/wslstart
+
