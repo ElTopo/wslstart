065cd05230c8d936f6c9bd569e3701fa974427f5
El Topo <eltopo@live.com>
2020-05-30 13:06:56 -0700

    add part of saving WSL's IP(s) in Windows wslip.txt file

--- user_wslstart.sh
+++ user_wslstart.sh
@@ -3,11 +3,24 @@
 # my ~/.wslstart.sh, which is executed by wslstart as normal user
 # this file should be soft-linked to ~/.wslstart.sh
 
+# get WSL's IP(s)
+WSLIP_FILE="/mnt/c/Users/lxl/wslip.txt"
+
+# get WSL's all IPs
+IPS=$(hostname -I)
+
+if [ -z "$IPS" ] ; then 
+	echo "Failed to get WSL's IP! quit."
+else
+	echo "WSL's IP(s): $IPS."
+fi
+echo "$IPS" > ${WSLIP_FILE}
+
+# for tightvncserver
 WSLUSER=$(whoami)
 VNCUSERDIR=/run/shm/${WSLUSER}-vnc
 DOTVNCDIR=~/.vnc
 
-# for tightvncserver
 if [ -x /usr/bin/tightvncserver ]
 then
 	mkdir -p $VNCUSERDIR
